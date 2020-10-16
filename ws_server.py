0b2285e0737fa1a556042d741709891584a3b161
Ecnavda <ecnavda@gmail.com>
2020-10-16 01:22:44 -0400

    Websocket server now sends messages to all clients.

--- ws_server.py
+++ ws_server.py
@@ -1,25 +1,50 @@
 import websockets
 import asyncio
 
+USER_LIST = set()
+
 
 async def hello(websocket, path):
     name = await websocket.recv()
-    print(f"< {name}")
-
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
+    print(name)
+    await websocket.send(name)
 
     while True:
         mesg = await websocket.recv()
         print(f"Received: {mesg}")
         await websocket.send(f"{mesg}")
 
-    print(f"< {greeting}")
+
+async def register_user(websocket):
+    USER_LIST.add(websocket)
+
+
+async def unregister_user(websocket):
+    USER_LIST.remove(websocket)
+
+
+async def notify_users(mesg):
+    if USER_LIST:
+        await asyncio.wait([user.send(mesg) for user in USER_LIST])
+
+
+async def entry_point(websocket, path):
+    # Register a user upon connecting
+    await register_user(websocket)
+    try:
+        greeting = await websocket.recv()
+        await notify_users(greeting)
+
+        while True:
+            mesg = await websocket.recv()
+            await notify_users(mesg)
+
+    finally:
+        await unregister_user(websocket)
 
 
 def run_server(port):
-    start_server = websockets.serve(hello, "localhost", port)
+    start_server = websockets.serve(entry_point, "localhost", port)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
