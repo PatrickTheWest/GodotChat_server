ae18db869b927185acffea7664cbe65f5f6858ed
Ecnavda <ecnavda@gmail.com>
2020-10-23 01:47:12 -0400

    All messages now carry username with the message. Added code to setup a secure connection if provided with certs.

--- ws_server.py
+++ ws_server.py
@@ -1,5 +1,7 @@
 import websockets
 import asyncio
+import ssl
+import pathlib
 import json
 
 
@@ -38,14 +40,35 @@ async def entry_point(websocket, path):
         await notify_users(greeting)
 
         while True:
-            mesg = await websocket.recv()
-            await notify_users(mesg)
+            mesg_recv = await websocket.recv()
+            mesg_send = process_json(mesg_recv)
+            await notify_users(mesg_send)
 
     finally:
         await unregister_user(websocket)
 
 
+def process_json(mesg):
+    message_contents = json.loads(mesg)
+    name = message_contents["name"]
+    message = message_contents["message"]
+    return f"{name}: {message}"
+
+
 def run_server(port):
+    """ For adding SSL uncomment
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    # Requires certificate chain
+    localhost_pem = pathlib.Path(__file__).with_name("fullchain.pem")
+    priv_key_pem = pathlib.Path(__file__).with_name("privkey.pem")
+    ssl_context.load_cert_chain(localhost_pem, priv_key_pem)
+
+    # Remove the ssl variable to run it insecurely
+    # Host field left blank to connect from ALL sources/connections
+    start_server = websockets.serve(entry_point, "", port, ssl=ssl_context)
+    """
+
+    # Comment this if running SSL
     start_server = websockets.serve(entry_point, "localhost", port)
 
     asyncio.get_event_loop().run_until_complete(start_server)
